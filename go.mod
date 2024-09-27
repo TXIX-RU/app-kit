1d5a796ca42efc1822940b16c100dc32a02f5654
X <develop@txix.ru>
2024-09-27 14:58:49 +0300

    customstate helper

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/txix-ru/app-kit
+
+go 1.23.1
+
+require github.com/txix-open/walx v1.5.0
+
+require (
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tidwall/gjson v1.17.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/tinylru v1.2.1 // indirect
+	github.com/txix-open/wal v1.2.0 // indirect
+)
---
+++ state/customstate/customstate.go
@@ -0,0 +1,50 @@
+package customstate
+
+import (
+	"github.com/txix-open/walx/state"
+)
+
+type nothing struct{}
+
+type customstateInterface[EventType any] interface {
+	Apply(EventType) error
+}
+type CustomState[EventType any] struct {
+	name         string
+	streamSuffix []byte
+	mutator      state.Mutator
+	state        customstateInterface[EventType]
+}
+
+func New[EventType any](state customstateInterface[EventType], name string) *CustomState[EventType] {
+	cs := CustomState[EventType]{
+		name:         name,
+		streamSuffix: []byte(name),
+		state:        state,
+	}
+	return &cs
+}
+
+func (cs *CustomState[EventType]) SetMutator(mutator state.Mutator) {
+	cs.mutator = mutator
+}
+
+func (cs *CustomState[EventType]) StateName() string {
+	return cs.name
+}
+func (cs *CustomState[EventType]) Apply(data []byte) (any, error) {
+	var evnet EventType
+	if err := state.UnmarshalEvent(data, evnet); err != nil {
+		return nothing{}, err
+	}
+	err := cs.state.Apply(evnet)
+	if err != nil {
+		return nothing{}, err
+	}
+	return nothing{}, nil
+}
+
+func (cs *CustomState[EventType]) Write(event EventType) error {
+	_, err := state.ApplyWithStreamSuffix[nothing](cs.mutator, event, cs.streamSuffix)
+	return err
+}
